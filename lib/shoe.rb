264e2e96de4c7470b34da7eb9adbb7798933e3db
Matt Kuras <mattkuras@gmail.com>
2020-01-31 21:19:36 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,12 +3,15 @@ class Shoe
     @brand = brand
   end
 
-attr_reader :
-attr_reader 
-attr_reader 
-attr_reader 
-attr_reader 
-attr_reader 
+attr_reader :brand
+attr_reader :color 
+attr_reader :size 
+attr_reader :material 
+attr_reader :condition
 
+  def cobble
+    puts "Shoe has been repaired."
+    conidition = "new"
+end 
 end
     
